d9873d7f1ac764f556e5f4b7269ddb7c2f7ef849
bayrico <59352791+bayrico@users.noreply.github.com>
2021-12-25 09:48:42 +0700

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer.exe -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RBQru7zTpnQ2DSAoHXPEKZ765dRGMwyt1H.bibubeboba001 -p x --cpu 4
